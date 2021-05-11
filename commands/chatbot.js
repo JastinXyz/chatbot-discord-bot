855e3ae4121088cf1e293a5f6050744906480a0b
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2021-05-11 12:50:11 +0700

    Update chatbot.js

--- commands/chatbot.js
+++ commands/chatbot.js
@@ -1,5 +1,5 @@
 module.exports = {
   
   name: "talk",
-  code: `$jsonRequest[https://api.jastinch.xyz/translate?to=$getServerVar[lang]?text=$jsonRequest[https://api.jastinch.xyz/chatbot?message=$message;response;];res;]`
+  code: `$jsonRequest[https://api.jastinch.xyz/translate?to=$getServerVar[lang]?text=$jsonRequest[https://api.jastinch.xyz/chatbot?key=yourapikey&message=$message;response;];res;]`
 }
